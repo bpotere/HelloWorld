6f7f2f32a88fe42e4927535719255e1c0173dea3
Bryan Potere <bpotere@gmail.com>
2015-02-05 13:04:41 -0500

    Initial commit

---
+++ AndroidStudioProjects/HelloWorld/app/src/androidTest/java/com/example/bryan/helloworld/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.bryan.helloworld;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&ved=0CD4QFjAD&url=http%3A%2F%2Fcapstone.cs.ucsb.edu%2Fcs189a%2Fsupport%2FSRS-template.doc&ei=t6nTVMjCB5OxyAS8jILABA&usg=AFQjCNHANPrwtmvNuca9gSBLdwhOra0Bgw
